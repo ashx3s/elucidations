c0b3719088e1ba746c103767a0f4bb114d2f2948
Ashlyn Knox <ashlyn@lilyx.io>
2024-09-21 22:14:44 -0600

    setup code for creating paths

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,15 @@
 // const postulateOne = canvas.draw();
 
 /* EXPERIMENT BASED ON SVGEDIT [Layer.js L95](https://github.com/SVG-Edit/svgedit) */
+
+/* GOAL: to toggle the visibility of an element */
+
+// get the svg element from the page
+
+// create a variable for the NS
+
+// TODO: create a layer Element NS
+
+// create an element NS and draw a point on it
+
+// appendChild the element to the svg
